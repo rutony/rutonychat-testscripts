2384515867d07ecd25c3f14e079356bd9d8b8df8
rutony <tonik19@gmail.com>
2021-02-25 00:36:12 +0300

    FormMain / IgnoreList , etc

---
+++ TestScripts/FormMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RutonyChat {
+    public partial class FormMain : Form {
+
+        public List<string> privList;
+        public List<string> ListFavorites;
+        public List<string> IgnoreList;
+
+        public static FormMain _instance;
+
+        public FormMain() {
+            InitializeComponent();
+        }
+    }
+}
