489efe842a5c3e854bad2d51bce87958d6e0e4fc
Bolshakov Anton <tonik19@gmail.com>
2021-08-19 16:45:44 +0300

    add send message

---
+++ TestScripts/RutonyChatFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RutonyChat {
+    public static class RutonyChatFunctions {
+
+        public static bool SendMessage(ProgramProps.SiteEnum _site, string _text) {
+
+            return true;
+
+        }
+    }
+}
